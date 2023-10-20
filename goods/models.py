5ac6634e57be1b4acec8ff580d4df1fa13365e3f
imanethepe <toquenathalie@gmail.com>
2023-10-20 16:44:34 +0200

    Starting of the project inventory

---
+++ goods/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+
+# Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=31, unique=True)
+    slug = models.SlugField(
+        max_length=31,
+        unique=True,
+        help_text='A label for URL conﬁg.')
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(
+        max_length=31, db_index=True)
+    slug = models.SlugField(
+         max_length=31,
+         unique=True,
+         help_text='A label for URL conﬁg.')
+    description = models.TextField()
+    estimated_price = models.CharField(
+        max_length=31, db_index=True)
+    entry_date = models.DateField(
+        'date of entry')
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    class Meta:
+        ordering = ['name']
+        get_latest_by = 'entry_date'
+
+    def __str__(self):
+        return self.name
